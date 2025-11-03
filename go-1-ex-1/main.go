e7aa8c3dfe76ccb5d7320013b18300eb3ce72e3b
NeklCast <160930376+NeklCast@users.noreply.github.com>
2025-11-03 15:00:23 +0100

    Aufgabe 1

--- go-1-ex-1/main.go
+++ go-1-ex-1/main.go
@@ -4,6 +4,18 @@ import "fmt"
 
 func main() {
 	// TODO: Declare and initialize the variables being used in the output!
+
+	// Aufgabe
+	var firstName string = "Nataliia"
+	var lastName string = "Kakhnych"
+	var dayOfBirth int = 7
+	var monthOfBirth int = 9
+	var yearOfBirth int = 2005
+	var numberOfSiblings int = 1
+	heightInMeters := 1.63
+	var zodiacSign = 'J'
+	// Aufgabe
+
 	fmt.Printf("Vor- und Nachname: %s %s\n", firstName, lastName)
 	fmt.Printf("Geburtsdatum: %d.%d.%d\n", dayOfBirth, monthOfBirth, yearOfBirth)
 	fmt.Printf("Anzahl Geschwister: %d\n", numberOfSiblings)
