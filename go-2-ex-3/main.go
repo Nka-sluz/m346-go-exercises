07da77671911f936481bb55c07bb40b6588d612c
Nka-sluz <nataliia_kakhnych@sluz.ch>
2025-11-17 15:08:55 +0100

    Aufgabe 2.3

--- go-2-ex-3/main.go
+++ go-2-ex-3/main.go
@@ -3,14 +3,19 @@ package main
 import "fmt"
 
 func main() {
-	// TODO: create a map called "modules"
+	modules := map[uint]string{
+		104: "Datenmodell implementieren",
+		117: "Informatik- und Netzinfrastruktur für ein kleines Unternehmen realisieren",
+		346: "Cloud Lösungen konzipieren und realisieren",
+	}
 
 	fmt.Println("Modul 104:", modules[104])
 	fmt.Println("Modul 117:", modules[117])
 	fmt.Println("Modul 346:", modules[346])
 
-	// TODO: delete one
-	// TODO: add one
-	// TODO: replace one
+	delete(modules, 346)
+	modules[320] = "Objektorientiert Programmieren"
+	modules[117] = "Informatik- und Netzinfrastruktur realisieren"
+
 	fmt.Println(modules)
 }
