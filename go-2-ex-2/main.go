f9040244fd5e6bd61023115ef3f544adf2774847
Nka-sluz <nataliia_kakhnych@sluz.ch>
2025-11-17 14:54:08 +0100

    Aufgabe 2.2

--- go-2-ex-2/main.go
+++ go-2-ex-2/main.go
@@ -6,10 +6,19 @@ func main() {
 	var fibs = []int{1, 1, 0, 0, 0}
 
 	fibs[2] = fibs[0] + fibs[1]
+	fibs[3] = fibs[1] + fibs[2]
+	fibs[4] = fibs[2] + fibs[3]
 	// TODO: correct up to index 4 using direct element access
 
-	fibs = append(fibs, 0) // TODO: replace 0 with the next Fibonacci number
+	fibs = append(fibs, fibs[3]+fibs[4]) // TODO: replace 0 with the next Fibonacci number
 	// TODO: compute three more Fibonacci numbers and append them
+	var newNumber int
+	newNumber = fibs[4] + fibs[5]
+	fibs = append(fibs, newNumber)
+	newNumber = fibs[5] + fibs[6]
+	fibs = append(fibs, newNumber)
+	newNumber = fibs[len(fibs)-1] + fibs[len(fibs)-2]
+	fibs = append(fibs, newNumber)
 
 	fmt.Println(fibs) // expected output: [1 1 2 3 5 8 13 21 34]
 }
