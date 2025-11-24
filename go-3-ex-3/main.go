65f4185a9f33dc63cc63ae4ba0f3d9aae4cc53b0
Nka-sluz <nataliia_kakhnych@sluz.ch>
2025-11-24 14:51:04 +0100

    Aufgabe 3.3

--- go-3-ex-3/main.go
+++ go-3-ex-3/main.go
@@ -1,10 +1,32 @@
 package main
 
+import "fmt"
+
 const (
 	Lower = 1
 	Upper = 30
 )
 
 func main() {
-	// TODO: Implement FizzBuzz using a for loop from Lower to Upper.
+	for i := Lower; i <= Upper; i++ {
+		toPrint := ""
+
+		switch {
+		case i%3 == 0:
+			toPrint += "Fizz"
+			if i%5 != 0 { // prevent unwanted Buzz
+				fmt.Println(toPrint)
+				break
+			}
+			fallthrough
+
+		case i%5 == 0:
+			toPrint += "Buzz"
+			fmt.Println(toPrint)
+
+		default:
+			fmt.Println(i)
+		}
+	}
+
 }
