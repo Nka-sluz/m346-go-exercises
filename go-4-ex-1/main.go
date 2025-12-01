e9aef86d74b9b756114316dfc03124119c74da85
Nka-sluz <nataliia_kakhnych@sluz.ch>
2025-12-01 14:23:43 +0100

    Aufgabe 4.1

--- go-4-ex-1/main.go
+++ go-4-ex-1/main.go
@@ -1,7 +1,28 @@
 package main
 
-// TODO: implement the function computeGrade
+import (
+	"errors"
+	"fmt"
+	"os"
+)
+
+func computeGrade(gotPoints int, maxPoints int) (float32, error) {
+	if gotPoints < 0 {
+		return 0.0, errors.New("got less than 0 points")
+	} else if gotPoints > maxPoints {
+		return 0.0, errors.New("got more than maximum points")
+	} else if maxPoints <= 0 {
+		return 0.0, errors.New("max points is less or equal 0")
+	}
+	res := (float32(gotPoints)/float32(maxPoints))*5 + 1
+	return res, nil
+}
 
 func main() {
-	// TODO: call the function computeGrade
+	grade, err := computeGrade(1, 6)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Grade could not be calculated: %v\n", err)
+	} else {
+		fmt.Printf("Final grade is %v\n", grade)
+	}
 }
